0a0df3c33b40efa818b748232443c8ff0fadb698
Matvey Chudakov <matvey159@gmail.com>
2022-05-01 15:52:17 +0300

    refactor microservices to kube

--- amigosservices/database/init.sql
+++ amigosservices/database/init.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE customer OWNER = postgres
+CREATE DATABASE customer OWNER = postgres;
 
-CREATE DATABASE fraud OWNER = postgres
+CREATE DATABASE fraud OWNER = postgres;
 
-CREATE DATABASE notification OWNER = postgres
+CREATE DATABASE notification OWNER = postgres;
